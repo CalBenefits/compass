1d19f5c595ebbef398a283c0a79b1f7cdbac6403
ycleptkellan <yclepkellan@gmail.com>
2017-03-26 19:29:32 -0700

    zip code translation fix

---
+++ public/db.js
@@ -0,0 +1,14 @@
+const config = {
+    apiKey: "AIzaSyDkStdhgEH45RGotVbSO6CakBFFYppiGLs",
+    authDomain: "cal-compass.firebaseapp.com",
+    databaseURL: "https://cal-compass.firebaseio.com",
+    storageBucket: "cal-compass.appspot.com",
+    messagingSenderId: "888186000127"
+};
+
+firebase.initializeApp(config);
+
+const rootRef = firebase.database().ref();
+
+return these;
+
